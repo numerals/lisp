7e8a938727f57f96606938c647aae1ac91c34f32
Sartaj Singh <singhsartaj94@gmail.com>
2014-05-10 20:28:37 +0530

    Polish notation

--- lispy.c
+++ lispy.c
@@ -5,7 +5,24 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
-int main() {
+// Including MPC lib
+#include "mpc.h"
+
+int main(int argc, char** argv) {
+
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+    mpca_lang(MPCA_LANG_DEFAULT, 
+            "                                                   \
+            number   : /-?[0-9]+/ ;                             \
+            operator : '+' | '-' | '*' | '/' ;                  \
+            expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+            lispy    : /^/ <operator> <expr>+ /$/ ;             \
+            ",
+            Number, Operator, Expr, Lispy);
 
     puts("Lispy Version 0.0.1\n");
     puts("Press Ctrl+c to exit\n");
@@ -16,11 +33,24 @@ int main() {
 
         add_history(input);
 
-        puts("yes");
+        mpc_result_t r;
+
+        if(mpc_parse("<stdin>", input, Lispy, &r)) {
+            /* On Success Print the AST */
+            mpc_ast_print(r.output);
+            mpc_ast_delete(r.output);
+
+        } else {
+            /* Otherwise Print the Error */
+            mpc_err_print(r.error);
+            mpc_err_delete(r.error);
+        }
 
         free(input);
 
     }
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
 
     return 0;
 }
