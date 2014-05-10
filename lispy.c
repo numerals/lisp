b305d258a7fbae89bb20a184572063de2ca5d6b3
Sartaj Singh <singhsartaj94@gmail.com>
2014-05-10 21:34:45 +0530

    Evaluating polish notation

--- lispy.c
+++ lispy.c
@@ -8,6 +8,9 @@
 // Including MPC lib
 #include "mpc.h"
 
+long eval(mpc_ast_t* t);
+long eval_op(long x, char* op, long y);
+
 int main(int argc, char** argv) {
 
     mpc_parser_t* Number = mpc_new("number");
@@ -20,7 +23,7 @@ int main(int argc, char** argv) {
             number   : /-?[0-9]+/ ;                             \
             operator : '+' | '-' | '*' | '/' ;                  \
             expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-            lispy    : /^/ <operator> <expr>+ /$/ ;             \
+            lispy    : /^/ <operator> <expr>+ /$/ ;       \
             ",
             Number, Operator, Expr, Lispy);
 
@@ -37,7 +40,8 @@ int main(int argc, char** argv) {
 
         if(mpc_parse("<stdin>", input, Lispy, &r)) {
             /* On Success Print the AST */
-            mpc_ast_print(r.output);
+            long result = eval(r.output);
+            printf("%ld\n", result);
             mpc_ast_delete(r.output);
 
         } else {
@@ -55,3 +59,43 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+long eval(mpc_ast_t* t) {
+
+    if(strstr(t->tag, "number")) {
+
+        return atoi(t->contents);
+
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+
+    while(strstr(t->children[i]->tag, "expr")) {
+
+        x = eval_op(x, op, eval(t->children[i]));
+        ++i;
+    }
+
+    return x;
+}
+
+long eval_op(long x, char* op, long y) {
+
+    if(!strcmp(op, "+")) {
+        return x + y;
+    }
+    else if(!strcmp(op, "-")) {
+        return x - y;
+    }
+    else if(!strcmp(op, "*")) {
+        return x * y;
+    }
+    else if(!strcmp(op, "/")) {
+        return x / y;
+    }
+    return 0;
+
+}
